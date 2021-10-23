f8077f50c37437e37124891993d8e1543705d938
Nahuel Ferreyra <ferreyranahuel746@gmail.com>
2021-10-22 22:56:46 -0300

    Primer commit JS

---
+++ js/variables.js
@@ -0,0 +1,9 @@
+
+let nombreUsuario =prompt ("Cúal es tu nombre?");
+alert("Hola "  +  nombreUsuario+" vamos a averiguar el área del rectangulo ");
+let base=prompt("Cuantos cm mide de base el rectángulo?");
+let baseNumber=Number(base);
+let altura=prompt("Cuanto cm mide de altura el rectángulo?");
+let alturaNumber=Number(altura)
+ console.log(baseNumber*alturaNumber);
+alert(nombreUsuario+" el rectángulo tiene un area de "+baseNumber*alturaNumber+" cm2." )
