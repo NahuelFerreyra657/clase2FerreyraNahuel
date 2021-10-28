f2529fc4903d5e1f3c56616174059d118181d542
Nahuel Ferreyra <ferreyranahuel746@gmail.com>
2021-10-27 23:39:01 -0300

    Tercer commit JS

--- js/clase2.js
+++ js/clase2.js
@@ -1,14 +1,12 @@
 let nombreUsuario =prompt ("Cúal es tu nombre?");
 alert("Hola "  +  nombreUsuario);
 const edad = Number (prompt ( nombreUsuario +"  cuántos años tenés? "));
-const generoManga = (prompt ("Qué te gustaria leer:shounen,shojo,yuri o yaoi?"))
+const generoManga = (prompt ("Qué te gustaria leer:shounen,shojo o yaoi?"))
  if ((edad <"18" && generoManga== "shounen") || (edad >"18" && generoManga== "shounen")){
 console.log("Te recomendamos que leas NARUTO");
  } else if ((edad <"18" && generoManga== "shojo")|| (edad >"18" && generoManga== "shojo")){
 console.log ("Te recomendamos que leas FRUITS BASKET ");
-}  if ((edad >="18") && ( generoManga== "yuri")) {
- console.log ("Te recomendamos que leas CITRUS ");
-} 
+}  
 
  if ((edad >="18") && (generoManga== "yaoi")) {
     console.log ("Te recomendamos que leas JUNJO ROMANTICA");
